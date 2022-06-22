f176e9e3a2e1b4192e7fd23931f478e86a4b53fb
Gyen Abubakar <gyen@walulel.com>
2022-06-22 13:06:22 +0000

    solved exercise: weather-forecast

--- weather-forecast/weather_forecast.go
+++ weather-forecast/weather_forecast.go
@@ -1,8 +1,12 @@
+// Package weather names a group of files under a namespace, in this case, all source code relating to weather.
 package weather
 
+// CurrentCondition stores the current weather condition.
 var CurrentCondition string
+// CurrentLocation stores the current location for which the weather will be forecast.
 var CurrentLocation string
 
+// Forecast provides weather information based on the location and weather condition.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
